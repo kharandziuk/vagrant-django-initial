e0c8a55d472c463d8c38b89a484615bc5cc494c4
Max Kharandziuk <max.kharandziuk@gmail.com>
2014-12-10 19:36:42 +0200

    celery sample

--- project/project/settings.py
+++ project/project/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third party
+    'djcelery',
 )
 
 MIDDLEWARE_CLASSES = (
